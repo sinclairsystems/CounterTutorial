b28535b961c5bd44fbcf6a6b7d1b0df6119b1884
sinclairsystems <c2@sussol.net>
2018-10-08 22:51:13 +1300

    Started tutorial

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return <h1>hello world</h1>; //this uses react.create element automatically
+  }
+}
+
+export default Counter; //what is the default for?
