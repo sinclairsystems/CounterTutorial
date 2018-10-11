bbdaccab9aff45b3fefca76d7655594717c96fbf
chris sinclair <chrissinclairsystems@gmail.com>
2018-10-12 09:43:00 +1300

    stage1 tutorial done

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   //state includes any data that this object needs
+
+  /*constructor(){
+    super(); //first we must call the parent class constructor(component) as this is a child class
+    this.handleIncrement = this.handleIncrement.bind(this)
+  }*/
+
   state = {
     count: 0,
     imageUrl: "https://picsum.photos/200",
@@ -27,6 +33,16 @@ class Counter extends Component {
     );
   }
 
+  handleIncrement = product => {
+    //this.state.count++;
+    console.log(product);
+    this.setState({ count: this.state.count + 1 }); // this is required to detect the state change unlike angular
+  };
+
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1 });
+  };
+
   render() {
     //jsx elements must have 1 parent element
     return (
@@ -35,18 +51,24 @@ class Counter extends Component {
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">increment</button>
+        <button
+          onClick={() => this.doHandleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          increment
+        </button>
         {this.state.tags.length === 0 && "please create new tags"}
         {this.renderTags()}
       </div>
     );
+
     //we have to use this map instead of a loop as JSX doesn't have loops
     //this uses react.create element automatically
   }
 
   getBadgeClasses() {
     let classes = "badge  m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary"; //WHAT IS THIS SHORT HANDING SYNTAX
+    classes += this.state.count === 0 ? "warning" : "primary"; //WHAT IS THIS SHORT HANDING SYNTAX changing color depending on number
     return classes;
   }
 
